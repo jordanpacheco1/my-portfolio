b1e5a555296a40948bfe1a383743859e02f24ce9
Jordan Pacheco <jordanrune@gmail.com>
2022-06-12 15:52:28 -0300

    progress-bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/global.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import NextNProgress from "nextjs-progressbar";
 import { appWithTranslation } from "next-i18next";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -10,6 +11,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      <NextNProgress
+        stopDelayMs={400}
+        options={{ easing: "ease", speed: 400 }}
+      />
       <Component className="main" {...pageProps} />
     </>
   );
